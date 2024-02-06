8988b511ea19af86acb3f15c766b0b495b92c2d0
Lucas Gabriel <lucasgsilva2102@gmail.com>
2024-02-06 12:41:15 -0300

    chore: add terraform steps

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+  backend "s3" {
+    bucket = "dev-cortes-online-terraform-state"
+    region = "us-east-1"
+    key    = "terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 module "users" {
   source         = "../../infra/users"
   environment    = var.environment
